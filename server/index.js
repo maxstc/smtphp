0cc01eb455b0db659315b4d564adda8736b0c06f
maxstc <a@a.a>
2025-04-18 23:20:02 -0700

    merged web and mail server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,36 @@
+/////MONGODB CONNECTION
 import { MongoClient } from "mongodb";
-import * as fs from "fs";
 const client = new MongoClient("mongodb://127.0.0.1:27017");
 client.connect();
 let db = client.db("smtphp").collection("addresses");
 
+/////SMTP SERVER
+import { SMTPServer } from "smtp-server";
+let options = {
+    name: "YouNaughtyNaughty",
+    hideSize: true,
+    hidePIPELINING: true,
+    hide8BITMIME: true,
+    hideSMTPUTF8: true,
+    onConnect: (session, callback) => {
+        db.insertOne({
+            address: session.remoteAddress,
+            timestamp: Date.now(),
+        });
+        return callback();
+    }
+}
+const server = new SMTPServer(options).listen(25);
+
+/////WEB SERVER
+//import * as https from "https";
 import * as http from "http";
 // const options = {
 //     key: fs.readFileSync("../crypt/key.pem"),
 //     cert: fs.readFileSync("../crypt/cert.pem"),
 // }
 
+import * as fs from "fs";
 let clientHtml = fs.readFileSync("../webclient/index.html");
 
 let addresses = "";
@@ -20,6 +41,7 @@ function refreshAddresses() {
     });
 }
 
+//https.createServer(options, (req, res) => {
 http.createServer((req, res) => {
     if (req.url === "/addresses") {
         res.writeHead(200);
